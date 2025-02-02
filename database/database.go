e966b432eb46691d0728d47cc2de0595d2d999fa
MnPutrav2 <mochnurdinv2@gmail.com>
2025-02-02 21:01:18 +0700

    Add env setting

--- database/database.go
+++ database/database.go
@@ -2,12 +2,21 @@ package database
 
 import (
 	"database/sql"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func InitDB() *sql.DB {
-	db, err := sql.Open("mysql", "_:_@tcp(_)/_")
+	godotenv.Load()
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+
+	db, err := sql.Open("mysql", "%s:%s@tcp(%s)/%s", dbUser, dbPass, dbHost, dbName)
 	if err != nil {
 		panic(err.Error())
 	}
